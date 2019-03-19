d2db754b850f2ec1acc95dab4b56de35620b50fe
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 16:04:08 +0530

    Create Low.java

---
+++ Low.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Low {
+	public static void main(String s[]){
+		int max,min;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+			
+		}
