ff20aa08348ad4c90fdfdd1dcd3563e48a1fd786
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 15:59:43 +0530

    Create Ranges.java

---
+++ Ranges.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Ranges
+{
+  public static void main(String args[])
+  {
+    int a;
+    	Scanner sc=new Scanner(System.in);
+    	a=sc.nextInt();
+    	if(a>=1&&a<=10)
+    	{
+    		System.out.println("yes");
+    	}
+    	else
+    	{
+    		System.out.println("no");
+    	}
+  }
+}
