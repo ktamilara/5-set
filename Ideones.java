22c36a10c41ab171375f03f32dc3c4ea81856c07
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 14:57:42 +0530

    Create Ideones.java

---
+++ Ideones.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import jimport java.util.*;
+class Ideones
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.print((scan.nextInt()+scan.nextInt()));
+	}
+}ava.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.print((scan.nextInt()+scan.nextInt()));
+	}
+}
