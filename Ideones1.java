acaea2af4068906b476dc4eee9bf955ee5eee606
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 15:49:34 +0530

    Update and rename Ideones.java to Ideones1.java

---
+++ Ideones1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideones1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,r;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		r=n+1;
+		System.out.println(r);
+	}
+}
